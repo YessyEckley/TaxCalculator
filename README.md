fb7c4c1506af94e440578a2c13a809e27c983b2d
Yessy Eckley <teameckleyfigueroa@gmail.com>
2018-05-08 00:06:24 -0600

    Added Comment
    Added more comments to the js and the Read Me files.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TaxCalculator
+
+This code was created by Yessy Eckley -- 05/07/2018
+
+This uses a simple algorithm to calculate the amount of tax 
+someone needs to pay based on brackets.
+
+It will also calculate fixed tax deductions based on the 
+rateArray without a levelArray.
+
+Please Enjoy this simplified solution that will not have your gears spinning.
--- TaxCalculator.js
+++ TaxCalculator.js
@@ -1,3 +1,14 @@
+/* 
+This code was created by Yessy Eckley -- 05/07/2018
+
+This uses a simple algorithm to calculate the amount of tax 
+someone needs to pay based on brackets.
+
+It will also calculate fixed tax deductions based on the 
+rateArray without a levelArray.
+
+Please Enjoy this simplified solution that will not have your gears spinning.
+*/
 
 function FedTaxCalulator(amount, levelArray, rateArray) {
   var totalTax = 0.00;
