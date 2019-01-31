bcc278aedb826a7a6744f27e7f5874555151e267
Yessy Eckley <YessyEckley@users.noreply.github.com>
2019-01-31 12:46:39 -0700

    Testing the iPad Pro
    Testing the iPad Pro - I want to know if i can edit files with
    Textastic and how it integrated with Working Copy

--- TaxCalculator.js
+++ TaxCalculator.js
@@ -1,4 +1,5 @@
 /* 
+
 This code was created by Yessy Eckley -- 05/07/2018
 
 This uses a simple algorithm to calculate the amount of tax 
@@ -8,6 +9,7 @@ It will also calculate fixed tax deductions based on the
 rateArray without a levelArray.
 
 Please Enjoy this simplified solution that will not have your gears spinning.
+
 */
 
 function FedTaxCalulator(amount, levelArray, rateArray) {
